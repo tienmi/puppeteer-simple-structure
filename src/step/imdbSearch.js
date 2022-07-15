d0b1751fb32b32502ff114203e5d8abc239d720c
lin <weiyandesign@gmail.com>
2022-07-15 10:10:52 +0800

    feat: update demo

--- src/step/imdbSearch.js
+++ src/step/imdbSearch.js
@@ -3,7 +3,7 @@ const { IMDB_SEARCH_BAR, PAGE_STATS, MOVIE_PAGE } = require('@/selector');
 const { MOVIE_KEYWORD } = require('@/config/inputData.js');
 
 module.exports = async function () {
-    await page.waitForSelector(IMDB_SEARCH_BAR, { timeout: 3000 });
+    await page.waitForSelector(IMDB_SEARCH_BAR, { timeout: 10000 });
     await page.type(IMDB_SEARCH_BAR, MOVIE_KEYWORD);
     await page.keyboard.press('Enter');
     await page.waitForSelector(PAGE_STATS);
